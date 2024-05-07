4f5ec29537689f20f11c93c9f8209f41833a9b08
Gorav Sharma <147341818+gorav07@users.noreply.github.com>
2024-05-07 20:18:42 +0530

    Add files via upload
    This is a responsive eCommerce  dashboard for seller

---
+++ orders.js
@@ -0,0 +1,27 @@
+const Orders = [
+{
+productName: 'Product 1', productNumber:'001', paymentStatus:'Due', shipping:'Pending'
+},
+
+{
+    productName: 'Product 2', productNumber: '002', paymentStatus: 'Paid', shipping: 'Delivered'
+},
+
+{
+    productName: 'Product 3', productNumber: '003', paymentStatus: 'Paid', shipping: 'Declined'
+},
+
+{
+    productName: 'Product 4' ,  productNumber: '004', paymentStatus: 'Due', shipping: 'Pending'
+},
+
+{
+    productName: 'Product 5', productNumber: '005', paymentStatus: 'Paid', shipping: 'Delivered'
+},
+{
+    productName: 'Product 6', productNumber: '006', paymentStatus: 'Paid', shipping: 'Declined'
+},
+{
+    productName: 'Product 7', productNumber: '007', paymentStatus: 'Paid', shipping: 'Delivered'
+}
+]
